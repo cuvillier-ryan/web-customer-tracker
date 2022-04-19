1efef860c7fe6cba47953c4b68b49addbfb059ec
cuvillier.ryan@gmail.com <cuvillier.ryan@gmail.com>
2022-04-18 20:25:24 -0500

    injected CustomerDAO into CustomerController

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -1,15 +1,32 @@
 package com.luv2code.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springdemo.dao.CustomerDAO;
+import com.luv2code.springdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+	// inject the DAO into the controller
+	@Autowired
+	private CustomerDAO customerDao;
+	
+	
 	@RequestMapping("/list")
 	public String listCustomers(Model model) {
+		
+		// get customers from the dao
+		List<Customer> customers = customerDao.getCustomers();
+		
+		// add the customers to the model
+		model.addAttribute("customers", customers); 
 		
 		return "list-customers";
 	}
